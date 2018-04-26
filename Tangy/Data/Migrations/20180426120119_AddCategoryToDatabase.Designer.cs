3f7ad469bc1910603380495a88419b217989650c
Scott <scott@DESKTOP-GTDO6PT>
2018-04-26 10:12:49 -0400

    Category

--- Tangy/Data/Migrations/20180426120119_AddCategoryToDatabase.Designer.cs
+++ Tangy/Data/Migrations/20180426120119_AddCategoryToDatabase.Designer.cs
@@ -11,9 +11,10 @@ using Tangy.Data;
 namespace Tangy.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180426120119_AddCategoryToDatabase")]
+    partial class AddCategoryToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
